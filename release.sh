b5bac0f713ba3841f8b0467fd46c43cbd248beb2
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-11-17 21:14:19 -0300

    v1

---
+++ release.sh
@@ -0,0 +1,8 @@
+
+yarn dist
+git add *
+git commit -m "v1"
+git tag -d v1
+git push origin --delete v1
+git tag -a v1 -m "v1"
+git push --follow-tags
--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,66 @@ import got from "got"
 const DISCORD_WEBHOOK_URL =
   "https://discord.com/api/webhooks/778404181051965450/ODUyAOpVGXIrBFRaU67-YnS_U7sRIHC74iYLB7HxqCN4AYxJgg6CnDJVsiP1kddrX-YT"
 
+interface Assignee {
+  id: number
+}
+
+interface User {
+  id: number
+}
+
+interface Comment {
+  id: number
+  user: User
+}
+
+const getAssigneesThatHaventCommented = (
+  assignees: Assignee[],
+  comments: Comment[]
+) => {
+  const assigneeIdsThatCommented = new Set(
+    comments.map(comment => comment.user.id)
+  )
+  return assignees.filter(
+    assignee => !assigneeIdsThatCommented.has(assignee.id)
+  )
+}
+
+const sendFirstCommentReminders = (assignes: Assignee[], _message: string) =>
+  Promise.all(
+    assignes.map(_assigne =>
+      got.post(DISCORD_WEBHOOK_URL, {
+        json: {
+          username: "Inguiçado",
+          avatar_url: "https://i.ytimg.com/vi/JVoZdm6h-Uo/hqdefault.jpg",
+          content: "The message to send",
+        },
+      })
+    )
+  )
+
 const main = async () => {
+  if (!github.context.payload.pull_request) {
+    return
+  }
+
   const githubToken = core.getInput("GITHUB_TOKEN")
 
   const octokit = github.getOctokit(githubToken)
 
-  const assignees = await octokit.issues.listAssignees(github.context.repo)
-
-  /* await octokit.issues.createComment({
+  const pullRequest = {
     ...github.context.repo,
     issue_number: github.context.payload.pull_request.number,
-    body: comment,
-  }) */
+  }
 
-  await got.post(DISCORD_WEBHOOK_URL, {
-    json: {
-      username: "Inguiçado",
-      avatar_url: "",
-      content: "The message to send",
-    },
-  })
-  //
+  const { data: assigness } = await octokit.issues.listAssignees(pullRequest)
+
+  const { data: comments } = await octokit.issues.listComments(pullRequest)
+
+  await sendFirstCommentReminders(
+    getAssigneesThatHaventCommented(assigness, comments),
+    "imbica jogador"
+  )
 }
 
 main().catch((error: Error) => core.setFailed(error.message))
